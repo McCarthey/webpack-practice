dd83af19c9b05118be45b483ee29e5584546b3f2
Randall <randall@duomai.com>
2019-04-09 15:52:44 +0800

    [add]file-loader to bundle image

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,21 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
+    entry: "./src/index.js",
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ["file-loader"]
             }
         ]
     }
-}
+};
