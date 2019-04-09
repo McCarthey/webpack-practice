c1455b1ba5fa56fc83d9deba549566076f5d07aa
Randall <randall@duomai.com>
2019-04-09 16:34:25 +0800

    [add]html-webpack-plugin & clean-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 import _ from 'lodash'
-import './style.css'
-import Icon from './my-image.jpg'
-import Data from './data.xml'
+import printMe from './print'
 
 function component() {
     let element = document.createElement('div')
+    let btn = document.createElement('button')
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-    element.classList.add('hello')
+    btn.innerHTML = 'Click here, and check your console'
+    btn.onclick = printMe
+
+    element.appendChild(btn)
     
-    let myIcon = new Image()
-    myIcon.src = Icon
-
-    element.appendChild(myIcon)
-    console.log(Data)
-
     return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,20 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: '管理输出'
+        })
+    ],
     output: {
-        filename: "bundle.js",
+        filename: "[name].bundle.js", // 根据入口名动态的生成bundle名
         path: path.resolve(__dirname, "dist")
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(png|jpg|svg|gif)$/,
-                use: ["file-loader"]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: ["file-loader"]
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: ["csv-loader"]
-            },
-            {
-                test: /\.xml$/,
-                use: ["xml-loader"]
-            }
-        ]
-    }
 };
